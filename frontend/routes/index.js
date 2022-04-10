fabeba540ebadd22961fec679b270d0220952ae3
Tal Derei <Tad222@lehigh.edu>
2022-04-10 19:57:18 -0400

    switching to React and Nextjs frameworks

--- frontend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// retrieve home page
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Front-Page' });
-});
-
-module.exports = router;
